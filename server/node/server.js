a17a11bc66912a0382c59e5c971125f5875a4a89
Jerick Coneras <jerick.coneras@gmail.com>
2021-09-19 07:21:11 +0800

    Changed implementation to Magpie API v2

--- server/node/server.js
+++ server/node/server.js
@@ -15,15 +15,15 @@ checkEnv();
 const generateHeaders = () => {
   return {
     'Content-Type': 'application/json',
-    'Authorization': 'Basic ' + Buffer.from(process.env.MAGPIE_PUBLISHABLE_KEY + ":").toString('base64'),
+    'Authorization': 'Basic ' + Buffer.from(process.env.MAGPIE_SECRET_KEY + ":").toString('base64'),
   }
 };
 
 const product = {
   name: 'Picsum photo',
-  unit_amount: 10000,
+  unit_amount: 2500,
   currency: 'php',
-  image: 'https://picsum.photos/280/320?random=4',
+  image: 'https://picsum.photos/280/320?random=1',
 };
 
 app.use(express.static(process.env.STATIC_DIR));
@@ -36,6 +36,7 @@ app.get('/', (req, res) => {
 app.get('/config', async (req, res) => {
   res.send({
     publicKey: process.env.MAGPIE_PUBLISHABLE_KEY,
+    secretKey: process.env.MAGPIE_SECRET_KEY,
     unitAmount: product.unit_amount,
     currency: product.currency,
   });
@@ -61,14 +62,14 @@ app.post('/create-checkout-session', async (req, res) => {
   const { quantity } = req.body;
 
   // You can choose which payment methods you want your customers to use
-  const paymentMethods = ['card', 'bdo', 'bpi', 'unionbank', 'alipay', 'gcash', 'paymaya', 'unionpay', 'wechat'];
+  const paymentMethods = ['card', 'bdo', 'bpi', 'metrobank', 'pnb', 'rcbc', 'unionbank', 'alipay', 'gcash', 'paymaya', 'unionpay', 'wechat'];
 
   // Create new Checkout Session for the order
   // Other optional params include:
   // [billing_address_collection] - to display billing address details on the page
   // [customer] - if you have an existing Magpie Customer ID
   // [customer_email] - lets you prefill the email input in the Checkout page
-  // For full details see https://gist.github.com/domdanao/f922ba3a8c5065de02ce933322852532
+  // For full details see https://developer.magpie.im/checkout/operation/Create_Checkout_Session_v2_sessions__post/
   const params = {
     currency: product.currency,
     payment_method_types: paymentMethods,
@@ -105,10 +106,14 @@ app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
 
 function checkEnv() {
   const publicKey = process.env.MAGPIE_PUBLISHABLE_KEY;
+  const secretKey = process.env.MAGPIE_SECRET_KEY;
   const domain = process.env.DOMAIN;
 
   if (!publicKey) {
     console.log("You must set your Magpie Publishable Key in the environment variables. Please see the README.");
+    process.exit(0);
+  } else if (!secretKey) {
+    console.log("You must set your Magpie Secret Key in the environment variables. Please see the README.");
     process.exit(0);
   } else if (!domain) {
     console.log("You must set your Domain URL in the environment variables. Please see the README.");
